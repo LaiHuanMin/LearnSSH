c55d68092840c8db59032b248135bc976a6afca3
laihuanmin <lai_huanmin@163.com>
2017-10-24 01:47:05 +0800

    好坑，被跟路径坑道了

---
+++ src/main/java/rm/rmrf/action/LoginAction.java
@@ -0,0 +1,8 @@
+package rm.rmrf.action;
+
+public class LoginAction{
+	public String execute()  throws Exception{
+		System.out.println("running");
+		return "ok";
+	}
+}
---
+++ src/main/java/rm/rmrf/test/Test.java
@@ -0,0 +1,11 @@
+package rm.rmrf.test;
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class.forName("org.apache.struts2.dispatcher.FilterDispatcher");
+	} 
+}  
+  
